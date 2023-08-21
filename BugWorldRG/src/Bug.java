b22baf706c072de7f9480e8925dfa288355e1ab8
ronangriffin <ronangriffin@localhost>
2023-08-21 14:28:47 +1200

    Created Bug class

---
+++ BugWorldRG/src/Bug.java
@@ -0,0 +1,71 @@
+
+public class Bug {
+	private String species;
+	private String name;
+	private char symbol;
+	private int x;
+	private int y;
+	private int energy;
+	private int idNum;
+	
+	public String getSpecies() {
+		return species;
+	}
+	public void setSpecies(String species) {
+		this.species = species;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public char getSymbol() {
+		return symbol;
+	}
+	public void setSymbol(char symbol) {
+		this.symbol = symbol;
+	}
+	public int getX() {
+		return x;
+	}
+	public void setX(int x) {
+		this.x = x;
+	}
+	public int getY() {
+		return y;
+	}
+	public void setY(int y) {
+		this.y = y;
+	}
+	public int getEnergy() {
+		return energy;
+	}
+	public void setEnergy(int energy) {
+		this.energy = energy;
+	}
+	public int getIdNum() {
+		return idNum;
+	}
+	public void setIdNum(int idNum) {
+		this.idNum = idNum;
+	}
+	public Bug(String species, String name, char symbol, int x, int y, int energy, int idNum) {
+		super();
+		this.species = species;
+		this.name = name;
+		this.symbol = symbol;
+		this.x = x;
+		this.y = y;
+		this.energy = energy;
+		this.idNum = idNum;
+	}
+	@Override
+	public String toString() {
+		return "Bug [species=" + species + ", name=" + name + ", symbol=" + symbol + ", x=" + x + ", y=" + y
+				+ ", energy=" + energy + ", idNum=" + idNum + "]";
+	}
+	
+	
+
+}
