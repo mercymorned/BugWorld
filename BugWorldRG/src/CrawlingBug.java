cc3c35816cd56f2887c4a67e4b13e814bd673dbc
ronangriffin <ronangriffin@localhost>
2023-08-23 14:56:55 +1200

    Created 3 bug subclasses

---
+++ BugWorldRG/src/CrawlingBug.java
@@ -0,0 +1,13 @@
+
+public class CrawlingBug extends Bug {
+
+	public CrawlingBug() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public CrawlingBug(String species, String name, char symbol, int x, int y, int energy, int idNum) {
+		super(species, name, symbol, x, y, energy, idNum);
+		// TODO Auto-generated constructor stub
+	}
+
+}
